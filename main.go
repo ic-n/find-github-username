235ffec594d71d9a9cdc54f9b8d6305640fd972a
Nikola Kiselev <ic.nikolai.kiselev@gmail.com>
2023-10-09 15:37:26 +0200

    docs: add read me and details for cli app

--- main.go
+++ main.go
@@ -14,34 +14,39 @@ import (
 
 var (
 	fOpts = &cli.StringSliceFlag{
-		Name: "opts",
+		Name:        "opts",
+		Required:    true,
+		Usage:       "Comma-separated list of possible parts in username",
+		DefaultText: "nice,cool,frog",
 	}
 	fIterations = &cli.IntFlag{
-		Name:    "iterations",
-		Value:   10,
-		Aliases: []string{"i"},
+		Name:        "iterations",
+		Value:       10,
+		Aliases:     []string{"i"},
+		Usage:       "Number of generation to test",
+		DefaultText: "10",
 	}
 	fConcats = &cli.IntFlag{
-		Name:    "concats",
-		Value:   3,
-		Aliases: []string{"c"},
+		Name:        "concats",
+		Value:       3,
+		Aliases:     []string{"c"},
+		Usage:       "Number of concat operations to username (how many times pick a chunk)",
+		DefaultText: "3",
 	}
 	fHypenChance = &cli.IntFlag{
-		Name:  "hypen",
-		Value: 100,
-		Usage: "% chance of hypen creation",
+		Name:        "hypen",
+		Value:       100,
+		Usage:       "% chance of hypen inserstion",
+		DefaultText: "100",
 	}
 )
 
 func main() {
 	app := &cli.App{
-		Name:   "boom",
-		Usage:  "make an explosive entrance",
-		Action: boom,
-		Flags: []cli.Flag{
-			fOpts,
-			fIterations, fConcats, fHypenChance,
-		},
+		Name:   "find",
+		Usage:  "find vacant github login",
+		Action: find,
+		Flags:  []cli.Flag{fOpts, fIterations, fConcats, fHypenChance},
 	}
 
 	if err := app.Run(os.Args); err != nil {
@@ -50,10 +55,9 @@ func main() {
 	}
 }
 
-func boom(c *cli.Context) error {
-	values := fOpts.Get(c)
+func find(c *cli.Context) error {
+	opts := fOpts.Get(c)
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	rs := func() string { return values[r.Intn(len(values))] }
 
 	var (
 		v string
@@ -62,7 +66,7 @@ func boom(c *cli.Context) error {
 
 	for i := 0; i < fIterations.Get(c); i++ {
 		for i := 0; i < fConcats.Get(c); i++ {
-			b.WriteString(rs())
+			b.WriteString(opts[r.Intn(len(opts))])
 			if r.Intn(100) < fHypenChance.Get(c) {
 				b.WriteRune('-')
 			}
--- readme.md
+++ readme.md
@@ -1,3 +1,47 @@
-# Find github username
+# GitHub Username Finder
 
-+ I found my new login by running `go run main.go -i=30 -c=2 -hypen=50 -opts="ic,n"`
+This command-line application allows you to find vacant GitHub account usernames by generating possible usernames based on a set of options and checking their availability on GitHub.
+
+## Usage
+
+To use the GitHub Username Finder, simply run the following command:
+
+```bash
+go run main.go --help
+```
+
+This will display the available options and commands for the application.
+
+__i.e.__: I found my new login (https://github.com/ic-n) by running `go run main.go -i=30 -c=2 -hypen=50 -opts="ic,n"`
+
+
+### Options
+
+- `--opts` (default: "nice,cool,frog"): Comma-separated list of possible parts in the username.
+- `--iterations`, `-i` (default: 10): Number of generations to test.
+- `--concats`, `-c` (default: 3): Number of concatenation operations to create the username.
+- `--hyphen` (default: 100): Percentage chance of hyphen insertion.
+
+## Example Usage
+
+Here's an example of how to use the GitHub Username Finder:
+
+```bash
+# Generate and check 10 usernames with default options
+go run main.go
+
+# Generate and check 20 usernames with custom options
+go run main.go --iterations 20 --concats 4 --hyphen 50 --opts "nice,cool,frog"
+```
+
+## Command
+
+- `help, h`: Shows a list of commands or help for one command.
+
+## License
+
+This GitHub Username Finder is open-source and available under the [MIT License](LICENSE).
+
+## Author
+
+This CLI application was developed by Nikolai Kiselev.
